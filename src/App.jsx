b11622eb032a5cbfd4c23522fb3f5fbbaf776d1f
Nobleman20 <emmanueloluyege@gmail.com>
2024-08-09 02:56:51 +0100

    Homepage rendered deploy fix 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.scss";
 import Home from "./pages/home/Home";
-import Register from "./pages/resgister/Register";
-import Watch from "./pages/watch/Watch";
+// import Register from "./pages/resgister/Register";
+// import Watch from "./pages/watch/Watch";
 
 function App() {
   return (
